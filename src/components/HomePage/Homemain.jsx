6947ce91220f15c1560b8589d8b6e99527261e12
Abhishek Anand <anand2000ys@gmail.com>
2023-05-04 22:15:34 +0530

    mainsection added

---
+++ src/components/HomePage/Homemain.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Homemain.css";
+
+const Homemain = () => {
+  return (
+    <div className="main">
+      <div className="main-container">
+        <div className="main-content">
+          <h3>
+            Medium is a place <br />
+            to write, read, and <br />
+            connect
+          </h3>
+          <h6>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
+          </h6>
+          <button>Start Writing</button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Homemain;
--- src/components/HomePage/Homepage.jsx
+++ src/components/HomePage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header/Header";
+import Homemain from "./Homemain";
 
 const Homepage = () => {
   return (
     <div>
       <Header />
+      <Homemain />
     </div>
   );
 };
