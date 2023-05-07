2e8fe9255e4bd76fa86ea70ce390ddd0f1c18854
Abhishek Anand <anand2000ys@gmail.com>
2023-05-07 21:11:04 +0530

    recommended section added

--- src/components/HomePage/Homepage.jsx
+++ src/components/HomePage/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
 import Homemain from "./Homemain";
+import Recommended from "../Recommended/Recommended";
 
 const Homepage = () => {
   return (
     <div>
       <Header />
       <Homemain />
+      <Recommended />
     </div>
   );
 };
