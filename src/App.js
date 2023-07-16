0900410c868dd0ff4efb20f89cd75bdee633f6bd
Abhishek <anand2000ys@gmail.com>
2023-07-16 17:27:44 +0530

    login and logout using redux and dispatchfunction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./components/HomePage/Homepage";
+import { useDispatch } from "react-redux";
+import { auth } from "./firebase";
+import { login } from "./features/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log({ authUser });
+        dispatch(
+          login({
+            providerData: authUser.providerData[0],
+          })
+        );
+      }
+    });
+  }, [dispatch]);
   return (
     <div className="App">
       <BrowserRouter>
