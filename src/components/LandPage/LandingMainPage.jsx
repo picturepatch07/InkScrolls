cacefa2877db41cf3adf0a30af3ac9de968d37c6
Abhishek <anand2000ys@gmail.com>
2023-07-21 16:43:45 +0530

    landing main page structure ready

--- src/components/LandPage/LandingMainPage.jsx
+++ src/components/LandPage/LandingMainPage.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LandingMainPage = () => {
+  const [tab, setTab] = useState(0);
+
   return (
     <div className="landing-main">
       <div className="landing-main-container">
@@ -12,6 +15,18 @@ const LandingMainPage = () => {
             >
               <span>FOLLOWING</span>
             </div>
+            <div
+              onClick={() => setTab(1)}
+              className={`tab ${tab === 1 && "active"}`}
+            >
+              <span>RECOMMENDED FOR YOU</span>
+            </div>
+          </div>
+          <div className="landing-write-story">
+            <h6>Share your ideas with millions of readers</h6>
+            <Link to="/new-story">
+              <button>Write on medium</button>
+            </Link>
           </div>
         </div>
       </div>
--- src/components/LandPage/index.js
+++ src/components/LandPage/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import LandHeader from "./LandHeader";
+import LandingMainPage from "./LandingMainPage";
 
 const index = () => {
   return (
     <div>
       <LandHeader />
+      <main>
+        <LandingMainPage />
+      </main>
     </div>
   );
 };
